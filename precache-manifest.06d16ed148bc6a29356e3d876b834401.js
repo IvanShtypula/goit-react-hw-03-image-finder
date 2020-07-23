52ad3ad63369caf711eb792aebc0538839dbe69f
Mypaxa4e <ivanshtypula@gmail.com>
2020-07-23 15:38:56 +0300

    Updates

---
+++ precache-manifest.06d16ed148bc6a29356e3d876b834401.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afaa73c103031dcf87933de404d6d32c",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "0d28b9ceb443474156aa",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.e91a957a.chunk.css"
+  },
+  {
+    "revision": "cef44f738718c520a413",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.b0433c44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.b0433c44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d28b9ceb443474156aa",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.688899fe.chunk.js"
+  },
+  {
+    "revision": "8cc29060e5a408f798eb",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.80fb11ad.js"
+  }
+]);
